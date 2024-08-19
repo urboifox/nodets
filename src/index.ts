495427ce6eea94837a26db8f747780c4bbb83601
fox <urboifox@gmail.com>
2024-08-19 04:04:26 +0300

    refactor: update tsconfig and general settings

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ app.get("/", (req: Request, res: Response) => {
     res.send("Hello, TypeScript + Node.js + Express!");
 });
 
-const port: number = 3000;
+const port = 3000;
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`🚀 Server started at http://localhost:${port}`);
 });
